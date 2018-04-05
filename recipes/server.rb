ef82eb7c9365a90c04230ba482fcfe7c44b54cb7
Manuel <manozo@example.com>
2018-04-05 17:41:14 +0000

    Release version 0.2.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,7 +5,11 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
 file '/var/www/html/index.html' do
-  content '<h1>Hello, world!</h1>'
+
+content "<h1>Hello, world!</h1>
+<h2>ipaddress: #{node['ipaddress']}</h2>
+<h2>hostname: #{node['hostname']}</h2>
+"
 end
 service 'httpd' do
   action [:enable, :start]
