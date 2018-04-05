ffbd79618e4e05345d9c37f4afaf115ff6109aba
Manuel <manozo@example.com>
2018-04-05 18:57:59 +0000

    Update default recipe to use template

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,13 +4,11 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 package 'httpd'
-file '/var/www/html/index.html' do
 
-content "<h1>Hello, world!</h1>
-<h2>ipaddress: #{node['ipaddress']}</h2>
-<h2>hostname: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+source 'index.html.erb'
 end
+
 service 'httpd' do
   action [:enable, :start]
 end
